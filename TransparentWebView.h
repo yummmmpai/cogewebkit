65a8733cecb9b9d8dcadb9e7cdf9f0f867a20f01
tamas.lov.nagy <tamas.lov.nagy@49df114e-257b-11df-bdc6-898f1b57464e>
2010-03-01 22:24:10 +0000

    - first commit on the new host

---
+++ TransparentWebView.h
@@ -0,0 +1,18 @@
+//
+//  TransparentWebView.h
+//  CoGeWebKit
+//
+//  Created by vade on 7/5/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <WebKit/WebKit.h>
+
+@interface TransparentWebView : WebView
+{
+
+}
+
+@end
